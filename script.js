62584f232549889e36675060848b249936a15603
andy <andy.ukpe@outlook.com>
2020-07-08 17:29:28 +0200

    Correzione errori + pulizia input + invio tramite tasto invio

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
   addToList();
+
   $(document).on('click', '.elimina', function() {
     var thisId = $(this).parent().attr('data-api-id');
     $.ajax(
       {
         url:'http://157.230.17.132:3032/todos/'+ thisId ,
-        method:'DELATE',
+        method:'DELETE',
         success: function(data){
           addToList();
 
@@ -18,26 +19,47 @@ $(document).ready(function() {
     )
   });
 
-    var newText = $('#todo-text').val();
-
-    if(newText.length > 0){
-      $.ajax(
-        {
-          url:'http://157.230.17.132:3032/todos/',
-          method:'POST',
-          success: function(data){
-            addToList();
-
-          },
-          error: function() {
-            alert('si è verificato un errore');
-          }
-      })
+  $('#todo-add').click(function(){
+    newToDo();
+  });
+  $('#todo-text').keypress(function(){
+    if(event.keyCode === 13 || event.which === 13){
+      newToDo();
     }
+
+  });
+    function newToDo() {
+
+
+      var newText = $('#todo-text').val();
+
+      if(newText.length > 0){
+        $.ajax(
+          {
+            url:'http://157.230.17.132:3032/todos/',
+            method:'POST',
+            data: {
+              text: newText,
+            },
+            success: function(data){
+              console.log(data);
+              addToList();
+
+            },
+            error: function() {
+              alert('si è verificato un errore');
+            }
+        })
+      }
+
+    }
+
 
   })
 
 function addToList(){
+  $('#todo-list').html('');
+  $('#todo-text').val('');
   $.ajax(
     {
       url:'http://157.230.17.132:3032/todos/',
@@ -45,7 +67,7 @@ function addToList(){
       success: function(data){
         console.log(data);
         if (data.length > 0) {
-          var source = $("entry-template").html();
+          var source = $("#entry-template").html();
           var template = Handlebars.compile(source);
 
           for (var i = 0; i < data.length; i++) {
